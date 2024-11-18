3c416ead548df1824f8e73cabf5c5a61b962158a
sandy0v0 <be0324ss@gmail.com>
2024-11-18 12:54:30 +0800

    完成重新上傳功能

--- update_img.php
+++ update_img.php
@@ -2,7 +2,7 @@
 include_once "function.php";
 
 $imgName=$_POST['imgName'];
-
+echo $imgName;
 if(isset($_FILES['img'])){
     if($_FILES['img']['error']==0){
         move_uploaded_file($_FILES['img']['tmp_name'],"./files/".$imgName);
