2f73518d61e176122c135f82f4efc8bcf377f31f
sandy0v0 <be0324ss@gmail.com>
2024-11-18 13:34:49 +0800

    建立檔案管理資料庫及資料表

--- update_img.php
+++ update_img.php
@@ -7,11 +7,10 @@ if(isset($_FILES['img'])){
     if($_FILES['img']['error']==0){
         move_uploaded_file($_FILES['img']['tmp_name'],"./files/".$imgName);
 
-}else{
-    echo "上傳失敗，請檢查檔案格式或是大小是否符合規定";
+    }else{
+        echo "上傳失敗，請檢查檔案格式或是大小是否符合規定";
     }
 }
 
 header("location:manage.php");
-
 ?>
