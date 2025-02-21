e6456d555cad23d7f1dd1825aa0c5e9fdd7bf369
sandy0v0 <be0324ss@gmail.com>
2025-02-21 11:28:06 +0800

    調整程式sql語法錯誤,完成csv上傳自動建表匯入功能

--- text-import.php
+++ text-import.php
@@ -34,16 +34,14 @@ if(!empty($_FILES['file'])){
     echo $_FILES['file']['name']."上傳成功";
     getfile("./files/{$_FILES['file']['name']}");
 }
-
-
 function getfile($path){
-    $file=fopen($path,'r');
+/*     $file=fopen($path,'r');
     $line=fgets($file);
-    $cols=explode(",",trim($line));
+    $header_cols=explode(",",trim($line));
     echo "<table>";
     echo "<tr class='header'>";
-    foreach($cols as $col){
-        echo "<th>{$col}</th>";
+    foreach($header_cols as $hc){
+        echo "<th>{$hc}</th>";
     }
     echo "</tr>";
 
@@ -55,10 +53,70 @@ function getfile($path){
         }
         echo "</tr>";
     }
+    fclose($file);
+    echo "</table>"; */
 
-    echo "</table>";
+    try {
+        // 使用PDO建立資料庫連線
+        $conn = new PDO("mysql:host=localhost;dbname=import", 'root', '');
+        // 設定PDO錯誤模式為拋出例外
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+        // 生成6位數字的隨機資料表名稱
+        $tableName = 'table_' . mt_rand(100000, 999999); // 6位亂數
+        $file=fopen($path,'r');
+        $line=fgets($file);
+        $header_cols=explode(",",trim($line));
+        // 創建資料表的SQL語法
+        $sql = "CREATE TABLE $tableName (
+            id INT AUTO_INCREMENT PRIMARY KEY,";
+            foreach($header_cols as $hc){
+                $tmpcols[]="$hc TEXT NOT NULL";
+            }
+            $sql .= join(",",$tmpcols);
+            $sql.=")";
+            echo $sql;
+        // 執行創建資料表的語句
+        $conn->exec($sql);
+        echo "資料表 $tableName 建立完成<br>";
+  
+        $first=false;
+        $count=0;
+        while($line=fgets($file)){
+            $tmp=[];
+            $tmp2=[];
+            $cols=explode(",",trim($line));
+            $sql="INSERT INTO $tableName (`";
+            foreach($header_cols as $hc){
+                $tmp[]=$hc;
+            }
+            $sql .= join("`,`",$tmp);
+            $sql .="`) values(";
+            
+            foreach($cols as $col){
+                $tmp2[]="'$col'";
+            }
+            $sql .= join(",",$tmp2);
+            
+            $sql .=")";
+
+            if($count<=2){
+                echo $sql;
+                echo "<br>";
+
+            }
+            
+            $conn->exec($sql);
+            $count++;
+        }
+        echo "資料匯入 $tableName 完成，共匯入 $count 筆資料";
+    fclose($file);
+
+
+    } catch(PDOException $e) {
+        echo "Error creating table: " . $e->getMessage();
+    }
 }
-
 
 ?>
 <!DOCTYPE html>
@@ -80,7 +138,7 @@ function getfile($path){
 
 
 <!----讀出匯入完成的資料----->
-<?php
+<?php 
 
 ?>
 
