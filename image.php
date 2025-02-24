a56f1ff419110578f71f8544276fa3c87f0cf6e0
sandy0v0 <be0324ss@gmail.com>
2025-02-24 10:00:18 +0800

    圖片放大功能+圖形加邊框+顏色

--- image.php
+++ image.php
@@ -62,12 +62,19 @@ if(!empty($_FILES['file']['tmp_name'])){
     $scale_big=1.5;
     $dst_small_width=$src_info[0]*$scale_small;
     $dst_small_height=$src_info[1]*$scale_small;
+    // 縮小↑/放大↓
+    $dst_big_width=$src_info[0]*$scale_big;
+    $dst_big_height=$src_info[1]*$scale_big;
 
     $dst_small=imagecreatetruecolor($dst_small_width,$dst_small_height);
     imagecopyresampled($dst_small,$src,0,0,0,0,$dst_small_width,$dst_small_height,$src_info[0],$src_info[1]);
 
+    $dst_big=imagecreatetruecolor($dst_big_width,$dst_big_height);
+    imagecopyresampled($dst_big,$src,0,0,0,0,$dst_big_width,$dst_big_height,$src_info[0],$src_info[1]);
+
     // 圖片抓出來後，要另外存成檔案為small03(記得寫路徑)
     imagejpeg($dst_small,"./images/small_{$_FILES['file']['name']}");
+    imagejpeg($dst_big,"./images/big_{$_FILES['file']['name']}");
     }
 
 ?>
@@ -83,12 +90,36 @@ if(!empty($_FILES['file']['tmp_name'])){
     </div>
     <div>
         <h3 class="text-center">放大</h3>
+        <img src="<?="./images/big_{$_FILES['file']['name']}";?>" alt="">
     </div>
 </div>
 
 
 <!----圖形加邊框----->
 
+<h2 class="text-center">圖形加邊框</h2>
+<?php
+$border=10;
+$bwidth=$src_info[0]+$border*2;
+$bheight=$src_info[1]+$border*2;
+
+$border_dst=imagecreatetruecolor($bwidth,$bheight);
+$border_color_red=imagecolorallocate($border_dst,255,0,0);
+$border_color_green=imagecolorallocate($border_dst,0,255,0);
+$border_color_blue=imagecolorallocate($border_dst,0,0,255);
+
+// rallocate可以選定一個顏色(依來源去訂為一個顏色)，
+imagefilledrectangle($border_dst,0,0,($bwidth/2),($bheight/2),$border_color_red);
+imagefilledrectangle($border_dst,0,($bheight/2),($bwidth/2),($bheight/2),$border_color_blue);
+imagefilledrectangle($border_dst,($bwidth/2),0,($bwidth/2),$bheight,$border_color_green);
+
+imagecopyresampled($border_dst,$src,$border,$border,0,0,$src_info[0],$src_info[1],$src_info[0],$src_info[1]);
+imagejpeg($border_dst,"./images/border_{$_FILES['file']['name']}");
+
+// 用矩形的方式去填邊框顏色，採一層一層堆疊上去的
+
+?>
+<img src="<?="./images/border_{$_FILES['file']['name']}";?>" alt="">
 
 <!----產生圖形驗證碼----->
 
